eceeac8516bd4cbf962e4198c5749fd8b52a0da9
rmateeq <rmateeq@yahoo.com>
2019-06-27 15:06:07 +0500

    Update udp-client.c

--- examples/rpl-udp/udp-client.c
+++ examples/rpl-udp/udp-client.c
@@ -30,7 +30,9 @@
 // int iat[5] = {};
 // int tp[4] = {};
 // int mt[4] = {};
-// 
+// #define NETSTACK_CONF_RDC_CHANNEL_CHECK_RATE 16
+// #define NETSTACK_CONF_RDC nullrdc_driver
+// #define NETSTACK_CONF_MAC nullmac_driver
 //>>my vars<<
 static struct simple_udp_connection udp_conn;
 
@@ -52,6 +54,7 @@ udp_rx_callback(struct simple_udp_connection *c,
   LOG_INFO_6ADDR(sender_addr);
 #if LLSEC802154_CONF_ENABLED
   LOG_INFO_(" LLSEC LV:%d", uipbuf_get_attr(UIPBUF_ATTR_LLSEC_LEVEL));
+  LOG_INFO(" RSSI:%f",packetbuf_attr(PACKETBUF_ATTR_RSSI));
 #endif
   LOG_INFO_("\n");
 
@@ -76,7 +79,32 @@ PROCESS_THREAD(udp_client_process, ev, data)
   int rd = NETSTACK_RADIO.set_value(RADIO_PARAM_TXPOWER, CC1200_CONST_TX_POWER_MIN);
 //  int rd = NETSTACK_RADIO.set_value(RADIO_PARAM_TXPOWER, -16);
 //update_txpower((int8_t)-16);
-  printf("tp state %d \n",rd);
+  printf("tp state: %d \n",rd);
+  int ch_val;
+  rd = NETSTACK_RADIO.get_value(RADIO_PARAM_CHANNEL, ch_val);
+  printf("channel state %d \n",rd);
+  printf("current channel: %d \n",ch_val);
+  int lqi_val;
+  rd = NETSTACK_RADIO.get_value(PACKETBUF_ATTR_LINK_QUALITY, lqi_val);
+  printf("lqi state %d \n",rd);
+  printf("current lqi: %d \n",lqi_val);
+  int rssi_val;
+  rd = NETSTACK_RADIO.get_value(RADIO_PARAM_RSSI, rssi_val);
+  printf("rssi state %d \n",rd);
+  printf("current rssi: %d \n",rssi_val);
+  int mt_val;
+  rd = NETSTACK_RADIO.get_value(CSMA_MAX_FRAME_RETRIES, mt_val);
+  printf("mt state %d \n",rd);
+  printf("current mt: %d \n",mt_val);
+  int minbe_val;
+  rd = NETSTACK_RADIO.get_value(CSMA_MIN_BE, minbe_val);
+  printf("minbe state %d \n",rd);
+  printf("current minbe: %d \n",minbe_val);
+  int maxbe_val;
+  rd = NETSTACK_RADIO.get_value(CSMA_MAX_BE, maxbe_val);
+  printf("maxbe state %d \n",rd);
+  printf("current maxbe: %d \n",maxbe_val);
+  printf("current buff: %d \n",PACKETBUF_SIZE);
 //  printf("tp after %d \n",RADIO_PARAM_TXPOWER);
 //  int val;
 //  NETSTACK_RADIO.get_value(RADIO_PARAM_TXPOWER, &val);
@@ -87,7 +115,7 @@ PROCESS_THREAD(udp_client_process, ev, data)
   simple_udp_register(&udp_conn, UDP_CLIENT_PORT, NULL,
     UDP_SERVER_PORT, udp_rx_callback);
   etimer_set(&periodic_timer, random_rand() % SEND_INTERVAL);
-  while(count <= 200 ) {
+  while(count <= 3 ) {
     PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&periodic_timer));
 
     if(NETSTACK_ROUTING.node_is_reachable() && NETSTACK_ROUTING.get_root_ipaddr(&dest_ipaddr)) {
