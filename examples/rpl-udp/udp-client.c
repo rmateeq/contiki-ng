b76cee2d668b6a3c221493acf467d6417ca9a14c
rmateeq <rmateeq@yahoo.com>
2019-06-27 15:55:25 +0500

    Update udp-client.c

--- examples/rpl-udp/udp-client.c
+++ examples/rpl-udp/udp-client.c
@@ -16,8 +16,8 @@
 #include "net/ipv6/simple-udp.h"
 
 #include "sys/log.h"
-#define LOG_MODULE "App"
-#define LOG_LEVEL LOG_LEVEL_INFO
+//#define LOG_MODULE "App"
+//#define LOG_LEVEL LOG_LEVEL_INFO
 
 #define WITH_SERVER_REPLY  1
 #define UDP_CLIENT_PORT 8765
