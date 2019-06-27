36fbb14f185655193310b21c11650ef174dbd499
rmateeq <rmateeq@yahoo.com>
2019-06-27 13:36:10 +0500

    Update udp-client.c

--- examples/rpl-udp/udp-client.c
+++ examples/rpl-udp/udp-client.c
@@ -5,7 +5,7 @@
 #include "cc1200-rf-cfg.h"
 #include "dev/radio.h"
 #include "node-id.h"
-#include "os/storage/cfs/cfs.h"
+//#include "os/storage/cfs/cfs.h"
 //>>my includes<<
 
 #include "contiki.h"
@@ -23,9 +23,10 @@
 #define UDP_SERVER_PORT 5678
 //<<set interval 10 instead of 60>>
 #define SEND_INTERVAL     (1 * CLOCK_SECOND)
- //>>set interval 10 instead of 60<<
+//>>set interval 10 instead of 60<<
 //<<my vars>>
-// int ps[4] = {};
+//int ps[4] = {39,74,109};
+//staticchar msg[40] = "abcdefghijklmnopqrstuvwxyzabcdefghijklm";
 // int iat[5] = {};
 // int tp[4] = {};
 // int mt[4] = {};
@@ -39,12 +40,12 @@ AUTOSTART_PROCESSES(&udp_client_process);
 /*---------------------------------------------------------------------------*/
 static void
 udp_rx_callback(struct simple_udp_connection *c,
-         const uip_ipaddr_t *sender_addr,
-         uint16_t sender_port,
-         const uip_ipaddr_t *receiver_addr,
-         uint16_t receiver_port,
-         const uint8_t *data,
-         uint16_t datalen)
+ const uip_ipaddr_t *sender_addr,
+ uint16_t sender_port,
+ const uip_ipaddr_t *receiver_addr,
+ uint16_t receiver_port,
+ const uint8_t *data,
+ uint16_t datalen)
 {
 
   LOG_INFO("Received response '%.*s' from ", datalen, (char *) data);
@@ -59,84 +60,62 @@ udp_rx_callback(struct simple_udp_connection *c,
 PROCESS_THREAD(udp_client_process, ev, data)
 {
   static struct etimer periodic_timer;
-  static unsigned count;
-  static char str[32];
+  static int count = 1;
+  static char str[40];
   uip_ipaddr_t dest_ipaddr;
-  int fp = -1;
- 
+
   PROCESS_BEGIN();
 
-  //<<file>>
-  struct cfs_dir dir;
-struct cfs_dirent dirent;
 
-if(cfs_opendir(&dir, "/groups/wall2-ilabt-iminds-be/") == 0) {
-  printf("dir open\n");
-  while(cfs_readdir(&dir, &dirent) != -1) {
-    printf("File: %s (%ld bytes)\n",
-           dirent.name, (long)dirent.size);
-  }
-  cfs_closedir(&dir);
-}
-  //int i;
-  //const char filepath[] = "/groups/wall2-ilabt-iminds-be/rt-mac/data/";
-  //const char nodeid[] = ;
-  //char sbuf[1024];
-  //sprintf (sbuf, "%s/%d", filepath, node_id);
-  fp = cfs_open ("/groups/wall2-ilabt-iminds-be/rt-mac/data/a.txt",CFS_APPEND);
-  if (fp >=0)
-    printf("success\n");
-  //>>file<<
- 
-  //<<set tx power>>
-  //printf("tp before %d \n",RADIO_PARAM_TXPOWER);
- int val1;
-  NETSTACK_RADIO.get_value(RADIO_PARAM_TXPOWER, &val1);
-  printf("....tp before %d \n",val1);
-  //radio_value_t tx_level = -16;
-  //int rd = NETSTACK_RADIO.set_value(RADIO_PARAM_TXPOWER, CC1200_CONST_TX_POWER_MIN);
-  int rd = NETSTACK_RADIO.set_value(RADIO_PARAM_TXPOWER, 0);
-  //update_txpower((int8_t)-16);
+//<<set tx power>>
+//printf("tp before %d \n",RADIO_PARAM_TXPOWER);
+//  int val1;
+//  NETSTACK_RADIO.get_value(RADIO_PARAM_TXPOWER, &val1);
+ // printf("....tp before %d \n",val1);
+//radio_value_t tx_level = -16;
+  int rd = NETSTACK_RADIO.set_value(RADIO_PARAM_TXPOWER, CC1200_CONST_TX_POWER_MIN);
+//  int rd = NETSTACK_RADIO.set_value(RADIO_PARAM_TXPOWER, -16);
+//update_txpower((int8_t)-16);
   printf("tp state %d \n",rd);
-  printf("tp after %d \n",RADIO_PARAM_TXPOWER);
-  int val;
-  NETSTACK_RADIO.get_value(RADIO_PARAM_TXPOWER, &val);
-  printf("....tp after %d \n",val);
-  //>>set tx power<<
-         
-  /* Initialize UDP connection */
+//  printf("tp after %d \n",RADIO_PARAM_TXPOWER);
+//  int val;
+//  NETSTACK_RADIO.get_value(RADIO_PARAM_TXPOWER, &val);
+//  printf("....tp after %d \n",val);
+//>>set tx power<<
+
+/* Initialize UDP connection */
   simple_udp_register(&udp_conn, UDP_CLIENT_PORT, NULL,
-                      UDP_SERVER_PORT, udp_rx_callback);
- //<<set tx power>>
-  //printf("tp after %d \n",RADIO_PARAM_TXPOWER);
-  //>>set tx power<<
+    UDP_SERVER_PORT, udp_rx_callback);
   etimer_set(&periodic_timer, random_rand() % SEND_INTERVAL);
-  while(1) {
+  while(count <= 200 ) {
     PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&periodic_timer));
 
     if(NETSTACK_ROUTING.node_is_reachable() && NETSTACK_ROUTING.get_root_ipaddr(&dest_ipaddr)) {
-      /* Send to DAG root */
-      LOG_INFO("Sending request %u to ", count);
+    /* Send to DAG root */
+      LOG_INFO("Sending request %d to ", count);
       LOG_INFO_6ADDR(&dest_ipaddr);
       LOG_INFO_("\n");
-      snprintf(str, sizeof(str), "hello %d", count);
-     //<<set tx power>>
-     //printf("tp after %d \n",RADIO_PARAM_TXPOWER);
-  //>>set tx power<<
+      if (count < 10)
+        snprintf(str, sizeof(str), "abcdefghijklmnopqrstuvwxyzabcdefghij00%d", count);
+      else if (count < 100)
+        snprintf(str, sizeof(str), "abcdefghijklmnopqrstuvwxyzabcdefghij0%d", count);
+      else
+        snprintf(str, sizeof(str), "abcdefghijklmnopqrstuvwxyzabcdefghij%d", count);
+   
       simple_udp_sendto(&udp_conn, str, strlen(str), &dest_ipaddr);
-      cfs_write (fp, str, sizeof(str));
- 
+  //  cfs_write (fp, str, sizeof(str));
+
       count++;
     } else {
       LOG_INFO("Not reachable yet\n");
     }
 
-    /* Add some jitter */
+  /* Add some jitter */
     etimer_set(&periodic_timer, SEND_INTERVAL
       - CLOCK_SECOND + (random_rand() % (2 * CLOCK_SECOND)));
   }
-    /* close the file*/  
-   cfs_close (fp);
+  /* close the file*/  
+// cfs_close (fp);
 
   PROCESS_END();
 }
