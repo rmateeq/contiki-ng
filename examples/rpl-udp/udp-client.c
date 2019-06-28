410e4f5178d32be65e8a05a51af3441e11e7cd1f
rmateeq <rmateeq@yahoo.com>
2019-06-28 05:59:53 +0500

    Update udp-client.c

--- examples/rpl-udp/udp-client.c
+++ examples/rpl-udp/udp-client.c
@@ -5,7 +5,7 @@
 #include "cc1200-rf-cfg.h"
 #include "dev/radio.h"
 #include "node-id.h"
-//#include "os/net/mac/csma/csma-output.c"
+#include "os/net/mac/csma/csma-output.c"
 #include "os/net/mac/csma/csma.h"
 
 //#include "os/storage/cfs/cfs.h"
