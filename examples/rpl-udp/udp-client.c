d4708ea80cc2b59170cd8319242b6235f627527e
rmateeq <rmateeq@yahoo.com>
2019-06-29 14:33:26 +0500

    Update udp-client.c

--- examples/rpl-udp/udp-client.c
+++ examples/rpl-udp/udp-client.c
@@ -135,7 +135,7 @@ PROCESS_THREAD(udp_client_process, ev, data)
  //packetbuf_set_attr(PACKETBUF_ATTR_MAX_MAC_TRANSMISSIONS,2);
  printf("current mt: %d \n",UIPBUF_ATTR_MAX_MAC_TRANSMISSIONS);
  printf("current mt: %d \n",PACKETBUF_ATTR_MAX_MAC_TRANSMISSIONS);
-  printf("current mt: %d \n",CSMA_MAX_FRAME_RETRIES); //1-UIP_MAX_MAC_TRANSMISSIONS_UNDEFINED (0),
+  //printf("current mt: %d \n",CSMA_MAX_FRAME_RETRIES); //1-UIP_MAX_MAC_TRANSMISSIONS_UNDEFINED (0),
  //2-UIPBUF_ATTR_MAX_MAC_TRANSMISSIONS,4- CSMA_MAX_FRAME_RETRIES
 /*  int minbe_val;
   rd = NETSTACK_RADIO.get_value(CSMA_MIN_BE, &minbe_val);
