4bfe6cc6cf4e9b6116ffbd21927a6cba4cd8979d
rmateeq <rmateeq@yahoo.com>
2019-07-01 13:59:21 +0500

    Update project-conf.h

--- examples/rpl-udp/project-conf.h
+++ examples/rpl-udp/project-conf.h
@@ -7,9 +7,13 @@
 //#define NETSTACK_CONF_RADIO   rf230_driver
 
 //#define RS232_BAUDRATE USART_BAUD_38400
-
+// CSMA MAC Parameters
 #define CSMA_CONF_MAX_BE 5
 #define CSMA_CONF_MIN_BE 0
 #define CSMA_CONF_MAX_FRAME_RETRIES 7
 
+// TSCH MAC Parameters
+#define TSCH_CONF_MAC_MAX_BE 5
+#define TSCH_CONF_MAC_MAX_BE 1
+#define TSCH_CONF_MAC_MAX_FRAME_RETRIES 7
 #endif /* PROJECT_CONF_H_ */
