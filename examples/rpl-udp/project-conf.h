9a8bd7d86d4eacfe46622e905570ddf9062de1cc
rmateeq <rmateeq@yahoo.com>
2019-07-01 14:38:47 +0500

    Update project-conf.h

--- examples/rpl-udp/project-conf.h
+++ examples/rpl-udp/project-conf.h
@@ -4,9 +4,16 @@
 //#define NETSTACK_CONF_RDC nullrdc_driver //
 #define NETSTACK_CONF_MAC nullmac_driver //tschmac_driver,csma_driver,sensniff_mac_driver
 //#define NETSTACK_CONF_FRAMER  framer_802154
-//#define NETSTACK_CONF_RADIO   rf230_driver
+//#define NETSTACK_CONF_RADIO   cc2538_rf_driver
 
 //#define RS232_BAUDRATE USART_BAUD_38400
+
+// Re-Mote channel and power configuration
+#ifndef CC2538_RF_CONF_CHANNEL
+#define CC2538_RF_CONF_CHANNEL 26
+#endif /* CC2538_RF_CONF_CHANNEL */
+//#define CC2538_RF_CONF_TX_POWER -40
+
 // CSMA MAC Parameters
 #define CSMA_CONF_MAX_BE 5
 #define CSMA_CONF_MIN_BE 0
