7f2d77f5d212b32e90c72a5f715e028f607a7c15
rmateeq <rmateeq@yahoo.com>
2019-07-01 13:51:36 +0500

    Update project-conf.h

--- examples/rpl-udp/project-conf.h
+++ examples/rpl-udp/project-conf.h
@@ -1,12 +1,12 @@
 #ifndef PROJECT_CONF_H_
 #define PROJECT_CONF_H_
 
-#define NETSTACK_CONF_RDC nullrdc_driver
-#define NETSTACK_CONF_MAC nullmac_driver
-#define NETSTACK_CONF_FRAMER  framer_802154
-#define NETSTACK_CONF_RADIO   rf230_driver
+//#define NETSTACK_CONF_RDC nullrdc_driver //
+#define NETSTACK_CONF_MAC nullmac_driver //tschmac_driver,csma_driver,sensniff_mac_driver
+//#define NETSTACK_CONF_FRAMER  framer_802154
+//#define NETSTACK_CONF_RADIO   rf230_driver
 
-#define RS232_BAUDRATE USART_BAUD_38400
+//#define RS232_BAUDRATE USART_BAUD_38400
 
 #define CSMA_CONF_MAX_BE 5
 #define CSMA_CONF_MIN_BE 0
