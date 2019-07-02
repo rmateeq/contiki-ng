9156cfcb4af753708077082d7995110b707a71dc
rmateeq <rmateeq@yahoo.com>
2019-07-02 10:38:38 +0500

    Update project-conf.h

--- examples/rpl-udp/project-conf.h
+++ examples/rpl-udp/project-conf.h
@@ -43,4 +43,12 @@
 // Transmissions power [4 options]
 //#define CC2538_RF_CONF_TX_POWER -40
 
+/***Logging***/
+#define LOG_CONF_LEVEL_IPV6                        LOG_LEVEL_DBG
+#define LOG_CONF_LEVEL_RPL                         LOG_LEVEL_DBG
+#define LOG_CONF_LEVEL_6LOWPAN                     LOG_LEVEL_DBG
+#define LOG_CONF_LEVEL_TCPIP                       LOG_LEVEL_DBG
+#define LOG_CONF_LEVEL_MAC                         LOG_LEVEL_DBG
+#define LOG_CONF_LEVEL_FRAMER                      LOG_LEVEL_DBG
+
 #endif /* PROJECT_CONF_H_ */
