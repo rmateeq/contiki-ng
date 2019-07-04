6476cae80ce93261f5b546763d2b8210df7dc1ca
rmateeq <rmateeq@yahoo.com>
2019-07-04 18:30:10 +0500

    Update project-conf.h

--- examples/rpl-udp/project-conf.h
+++ examples/rpl-udp/project-conf.h
@@ -65,12 +65,12 @@
 #define CC2538_RF_CONF_TX_POWER 0xFF
 //#endif
 /***Logging***/
-#define LOG_CONF_LEVEL_IPV6                        LOG_LEVEL_DBG
+//#define LOG_CONF_LEVEL_IPV6                        LOG_LEVEL_DBG
 #define LOG_CONF_LEVEL_RPL                         LOG_LEVEL_DBG
-#define LOG_CONF_LEVEL_6LOWPAN                     LOG_LEVEL_DBG
-#define LOG_CONF_LEVEL_TCPIP                       LOG_LEVEL_DBG
+//#define LOG_CONF_LEVEL_6LOWPAN                     LOG_LEVEL_DBG
+//#define LOG_CONF_LEVEL_TCPIP                       LOG_LEVEL_DBG
 #define LOG_CONF_LEVEL_MAC                         LOG_LEVEL_DBG
-#define LOG_CONF_LEVEL_FRAMER                      LOG_LEVEL_DBG
+//#define LOG_CONF_LEVEL_FRAMER                      LOG_LEVEL_DBG
 //#define LOG_LEVEL_NULLNET                          LOG_LEVEL_DBG
 //#define LOG_LEVEL_6TOP                             LOG_LEVEL_DBG
 //#define LOG_LEVEL_LWM2M                            LOG_LEVEL_DBG
