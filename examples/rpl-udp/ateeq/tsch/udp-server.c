c05d64ee4c6684e11fd15bc9b23a3f6220426b4a
rmateeq <rmateeq@yahoo.com>
2019-07-12 01:18:07 +0500

    Update udp-server.c

--- examples/rpl-udp/ateeq/tsch/udp-server.c
+++ examples/rpl-udp/ateeq/tsch/udp-server.c
@@ -7,7 +7,7 @@
 #define LOG_MODULE "App"
 #define LOG_LEVEL LOG_LEVEL_INFO
 
-#define WITH_SERVER_REPLY  1
+#define WITH_SERVER_REPLY  0
 #define UDP_CLIENT_PORT	8765
 #define UDP_SERVER_PORT	5678
 
