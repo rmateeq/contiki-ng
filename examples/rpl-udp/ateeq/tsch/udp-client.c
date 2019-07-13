82ed3edf832a19a941ce184dda6c73a0358de5ca
rmateeq <rmateeq@yahoo.com>
2019-07-14 00:45:11 +0500

    Update udp-client.c

--- examples/rpl-udp/ateeq/tsch/udp-client.c
+++ examples/rpl-udp/ateeq/tsch/udp-client.c
@@ -26,11 +26,11 @@
 //<<my vars>>
 unsigned long ct_start;
 //unsigned long ct_end;
-static int tp[4] = [-11,-5,1,7]; //[-13,-9,-5,-1,1,3,5];
-static int ps[2] = [25,100]; //[25,50,75,100];
-static int mt[2] = [1,5]; //[1,2,3,4,5];
+static int tp[4] = {-11,-5,1,7}; //[-13,-9,-5,-1,1,3,5];
+static int ps[2] = {25,100}; //[25,50,75,100];
+static int mt[2] = {1,5}; //[1,2,3,4,5];
 //bidirectional:yes,no
-static int iat[3] = [2,6,10]; //[1,2,4,6,8,10];
+static int iat[3] = {2,6,10}; //[1,2,4,6,8,10];
 //number of nodes: 8(d,s),16(d,s),24,32
 //dt: real
 //mac: tsch,mac
