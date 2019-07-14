3cba54fbf98868a76795e2abf8f12e4194bdc668
rmateeq <rmateeq@yahoo.com>
2019-07-15 04:04:40 +0500

    Update udp-server.c

--- examples/rpl-udp/ateeq/tsch/udp-server.c
+++ examples/rpl-udp/ateeq/tsch/udp-server.c
@@ -26,9 +26,19 @@ udp_rx_callback(struct simple_udp_connection *c,
          const uint8_t *data,
          uint16_t datalen)
 {
-  LOG_INFO("Received request '%.*s' from ", datalen, (char *) data);
+  //LOG_INFO("Received request '%.*s' from ", datalen, (char *) data);
+  printf("Received request '%.*s' from ", datalen, (char *) data);
   LOG_INFO_6ADDR(sender_addr);
-  LOG_INFO_("\n");
+  printf("\n");
+  //LOG_INFO_("\n");
+  int lqi_val;
+  int rd = NETSTACK_RADIO.get_value(RADIO_PARAM_LAST_LINK_QUALITY, &lqi_val);
+  printf("lqi-state: %d::",rd);
+  printf("lqi: %d::",lqi_val);
+  int rssi_val;
+  rd = NETSTACK_RADIO.get_value(RADIO_PARAM_LAST_RSSI, &rssi_val); //RADIO_PARAM_LAST_RSSI, RADIO_PARAM_LAST_PACKET_TIMESTAMP
+  printf("rssi-state: %d::",rd);
+  printf("rssi: %d::\n",rssi_val);
 #if WITH_SERVER_REPLY
   /* send back the same string to the client as an echo reply */
   LOG_INFO("Sending response.\n");
@@ -48,24 +58,9 @@ PROCESS_THREAD(udp_server_process, ev, data)
    LOG_INFO("current tp server:::: %d\n",tp_val);
   /* Initialize DAG root */
          
-    if ((clock_seconds() - ct_start) >= 100){
-           printf("restarting1\n\n\n");
-           PROCESS_RESTART();
-  }
-
-  NETSTACK_ROUTING.root_start();
-  if ((clock_seconds() - ct_start) >= 100){
-           printf("restarting2\n\n\n");
-           PROCESS_RESTART();
-  }
-
   /* Initialize UDP connection */
   simple_udp_register(&udp_conn, UDP_SERVER_PORT, NULL,
                       UDP_CLIENT_PORT, udp_rx_callback);
-  if ((clock_seconds() - ct_start) >= 100){
-           printf("restarting3\n\n\n");
-           PROCESS_RESTART();
-  }
   
   PROCESS_END();
 }
