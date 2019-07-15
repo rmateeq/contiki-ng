5541b865c3b9fd11edb023170c192ca936616e51
rmateeq <rmateeq@yahoo.com>
2019-07-15 12:55:10 +0500

    Update project-conf.h

--- examples/rpl-udp/ateeq/tsch/project-conf.h
+++ examples/rpl-udp/ateeq/tsch/project-conf.h
@@ -89,7 +89,7 @@
 //#endif
 /***Logging***/
 //#define LOG_CONF_LEVEL_IPV6                        LOG_LEVEL_INFO
-//#define LOG_CONF_LEVEL_RPL                         LOG_LEVEL_INFO
+#define LOG_CONF_LEVEL_RPL                         LOG_LEVEL_INFO
 //#define LOG_CONF_LEVEL_6LOWPAN                     LOG_LEVEL_DBG
 //#define LOG_CONF_LEVEL_TCPIP                       LOG_LEVEL_DBG
 //#define LOG_CONF_LEVEL_MAC                         LOG_LEVEL_DBG
