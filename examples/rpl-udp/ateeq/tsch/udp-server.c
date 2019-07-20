f0f256ecea6220de79c44291aed6f80b4c77e4fb
rmateeq <rmateeq@yahoo.com>
2019-07-21 03:22:13 +0500

    Update udp-server.c

--- examples/rpl-udp/ateeq/tsch/udp-server.c
+++ examples/rpl-udp/ateeq/tsch/udp-server.c
@@ -17,7 +17,7 @@ static struct simple_udp_connection udp_conn;
 static unsigned long ct_start;
 static struct etimer reset_timer;
 //static int conf_num = 48;
-static int tp[4] = {-7,-3,1,5};
+static int tp[5] = {-3,-1,1,3,5};
 static int tp_c = 0;
 static int run_time = 602; //600
 static int num_conf = 36;
