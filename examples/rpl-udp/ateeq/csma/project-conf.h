911592d3215da45c003580cac09a9f299dfbcdf6
rmateeq <rmateeq@yahoo.com>
2019-07-21 14:50:37 +0500

    Update project-conf.h

--- examples/rpl-udp/ateeq/csma/project-conf.h
+++ examples/rpl-udp/ateeq/csma/project-conf.h
@@ -1,1 +1,62 @@
+#ifndef PROJECT_CONF_H_
+#define PROJECT_CONF_H_
+//#define PACKETBUF_CONF_SIZE in bytes
+//RADIO_CONST_MAX_PAYLOAD_LEN, MAX_PAYLOAD_LEN, TSCH_PACKET_MAX_LEN
+//MAX_NUM_QUEUED_PACKETS,
 
+#define UIP_CONF_UDP_CONNS 30
+//Enforce MAC_MAX_TRANSMISSIONS across all nodes
+#define UIP_CONF_TAG_TC_WITH_VARIABLE_RETRANSMISSIONS 1
+
+/*** Network Layer ***/
+// Parameters [2 options]
+//#define UIP_CONF_IPV6 1
+//#ifndef NBR_TABLE_CONF_MAX_NEIGHBORS
+#define NBR_TABLE_CONF_MAX_NEIGHBORS 10 //default 16
+//#endif
+
+
+#define UIP_CONF_BUFFER_SIZE 165
+//#define UIP_CONF_IPV6_QUEUE_PKT 1
+
+
+//#define UIP_CONF_IPV6_RPL 1
+//-----------------------------------------------------------------------------
+ #define RPL_CONF_SUPPORTED_OFS {&rpl_of0, &rpl_mrhof}
+ #define RPL_CONF_OF_OCP RPL_OCP_OF0
+ #define RPL_CONF_TRICKLE_REFRESH_DAO_ROUTES 8 //by default dao refreshal is done every 4secs.
+ #define RPL_CONF_DIO_INTERVAL_MIN 13 //results in 8sec. default is 12, resulting in 4 sec.
+ #define RPL_CONF_DEFAULT_LIFETIME_UNIT 60 //defualt redefined due to confusion in rpl-conf.h
+ #define RPL_CONF_DEFAULT_LIFETIME 30 // same as above
+ #define RPL_CONF_DAO_RETRANSMISSION_TIMEOUT (10 * CLOCK_SECOND) //default 5
+// #define RPL_CONF_PROBING_SEND_FUNC
+// #define LINK_STATS_CONF_INIT_ETX_FROM_RSSI
+// #define RPL_MRHOF_CONF_SQUARED_ETX
+
+/*** MAC Layer ***/
+// select MAC protocol [2+1 options]
+#define NETSTACK_CONF_MAC csma_driver //tschmac_driver  //nullmac_driver,
+#define SICSLOWPAN_CONF_FRAG 0
+
+// CSMA MAC Parameters [1-4-3 options]
+#define QUEUEBUF_CONF_NUM 4 //default 8
+//#define RS232_BAUDRATE USART_BAUD_38400
+
+/*** PHY Layer ***/
+// Transmissions power [4 options] 0x00(-24),42(-15),58(-13),62(-11),72(-9),88(-7),91(-5),A1(-3),B0(-1),B6(0),C5(1),D5(3),ED(5),FF(7)
+//#ifndef CC2538_RF_CONF_TX_POWER
+//#define CC2538_RF_CONF_TX_POWER 0xC5
+#define ENERGEST_CONF_ON 1
+//#endif
+/***Logging***/
+//#define LOG_CONF_LEVEL_IPV6                        LOG_LEVEL_INFO
+#define LOG_CONF_LEVEL_RPL                         LOG_LEVEL_DBG
+//#define LOG_CONF_LEVEL_6LOWPAN                     LOG_LEVEL_DBG
+//#define LOG_CONF_LEVEL_TCPIP                       LOG_LEVEL_DBG
+#define LOG_CONF_LEVEL_MAC                         LOG_LEVEL_DBG
+//#define LOG_CONF_LEVEL_FRAMER                      LOG_LEVEL_DBG
+//#define LOG_LEVEL_NULLNET                          LOG_LEVEL_DBG
+//#define LOG_LEVEL_6TOP                             LOG_LEVEL_DBG
+//#define LOG_LEVEL_LWM2M                            LOG_LEVEL_DBG
+//#define LOG_LEVEL_MAIN                             LOG_LEVEL_DBG
+#endif /* PROJECT_CONF_H_ */
