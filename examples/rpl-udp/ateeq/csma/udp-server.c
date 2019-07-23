46a5fabb8fc8c91afc71ef0a48fd5d82fbb475d3
rmateeq <rmateeq@yahoo.com>
2019-07-24 04:48:02 +0500

    Update udp-server.c

--- examples/rpl-udp/ateeq/csma/udp-server.c
+++ examples/rpl-udp/ateeq/csma/udp-server.c
@@ -138,7 +138,7 @@ udp_rx_callback(struct simple_udp_connection *c, const uip_ipaddr_t *sender_addr
 PROCESS_THREAD(udp_server_process, ev, data)
 {
   PROCESS_BEGIN();
-  printf("Rtimer: %lu", (unsigned long) ((double)RTIMER_NOW()/(double)RTIMER_SECOND*100));
+  printf("Rtimer: %lu", (unsigned long) ((double)RTIMER_NOW()/(double)RTIMER_SECOND*1000));
   printf("Timer start (clock time): %lu \n", clock_time());
    printf("Timer start (clock sec): %lu \n", clock_seconds());
   printf("Tiicks per sec: %d \n", CLOCK_SECOND);
@@ -175,7 +175,7 @@ PROCESS_THREAD(udp_server_process, ev, data)
       /* Initialize UDP connection */
       simple_udp_register(&udp_conn, UDP_SERVER_PORT, NULL,
                           UDP_CLIENT_PORT, udp_rx_callback);
-printf("Rtimer: %lu", (unsigned long) (RTIMER_NOW()/RTIMER_SECOND*100));
+printf("Rtimer: %lu", (unsigned long) ((double) RTIMER_NOW()/(double) RTIMER_SECOND*1000));
       etimer_set(&reset_timer, (run_time*CLOCK_SECOND*num_conf));
       PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&reset_timer));
 //      printf("\nM__PKTSRECVD,%d:-:CONFNUM,%d\n",per_conf_counter,conf_count+(tp_c*num_conf));
