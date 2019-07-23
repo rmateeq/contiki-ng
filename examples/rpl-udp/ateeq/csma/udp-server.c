9eb892baa76de8dfd9c2d67db61a3d696ed3b0e4
rmateeq <rmateeq@yahoo.com>
2019-07-23 06:12:36 +0500

    Update udp-server.c

--- examples/rpl-udp/ateeq/csma/udp-server.c
+++ examples/rpl-udp/ateeq/csma/udp-server.c
@@ -5,6 +5,7 @@
 #include "net/ipv6/simple-udp.h"
 #include "net/ipv6/uiplib.h"
 #include "random.h"
+#include "sys/energest.h"
 #include "sys/log.h"
 #define LOG_MODULE "App"
 #define LOG_LEVEL LOG_LEVEL_INFO
@@ -14,16 +15,20 @@
 #define UDP_SERVER_PORT 5678
 
 static struct simple_udp_connection udp_conn;
-static unsigned long ct_start;
+//static unsigned long ct_start;
 static struct etimer reset_timer;
 //static int conf_num = 48;
-static int tp[5] = {-3,-1,1,3,5};
-static int tp_c = 0;
-static int run_time = 602; //600
-static int num_conf = 36;
+//int tp[4] = {5,3,1,-1};//{7,5,3,1,-1};
+//int tp_c = 0;
+int run_time = 630;//609;
+const int num_conf = 27;
 static int counter = 0;
+int tp_val = 1;
+//int per_conf_counter = 0;
+const int run_delay = 20;
+//int skew_pad = 0;
 static struct etimer reset_timer;
-
+//int conf_count = 1;
 PROCESS(udp_server_process, "UDP server");
 AUTOSTART_PROCESSES(&udp_server_process);
 /*---------------------------------------------------------------------------*/
@@ -68,6 +73,28 @@ int extractCount(
   return count;
 }
 /*---------------------------------------------------------------------------*/
+static unsigned long to_seconds(uint64_t time)
+{
+  return (unsigned long)(time / ENERGEST_SECOND);
+}
+/*---------------------------------------------------------------------------*/
+
+static void log_energy()
+{
+  energest_flush();
+
+  printf("\nM__RUNEnergest(s):\n");
+
+  printf("E__RCPU,%4lu:-:E__LPM,%4lu:-:E__DEEPLPM,%4lu:-:E__TotalTime,%lu\n",
+    to_seconds(energest_type_time(ENERGEST_TYPE_CPU)), to_seconds(energest_type_time(ENERGEST_TYPE_LPM)),
+    to_seconds(energest_type_time(ENERGEST_TYPE_DEEP_LPM)), to_seconds(ENERGEST_GET_TOTAL_TIME()));
+  
+  printf("E__RadioLISTEN,%4lu:-:E__TRANSMIT,%4lu:-:E__OFF,%4lu\n",
+    to_seconds(energest_type_time(ENERGEST_TYPE_LISTEN)), to_seconds(energest_type_time(ENERGEST_TYPE_TRANSMIT)),
+    to_seconds(ENERGEST_GET_TOTAL_TIME() - energest_type_time(ENERGEST_TYPE_TRANSMIT)
+        - energest_type_time(ENERGEST_TYPE_LISTEN)));
+}
+/*---------------------------------------------------------------------------*/
 static void
 udp_rx_callback(struct simple_udp_connection *c, const uip_ipaddr_t *sender_addr,
          uint16_t sender_port, const uip_ipaddr_t *receiver_addr, 
@@ -78,6 +105,7 @@ udp_rx_callback(struct simple_udp_connection *c, const uip_ipaddr_t *sender_addr
   uint64_t remote_time_clock_ticks = extractNetworkUptime(datalen, (char *) data);
   const int countExtracted = extractCount(datalen, (char *) data);
   counter++;
+  //per_conf_counter++;
   printf("\nD__SEQNO,%d:-:",countExtracted);
   char buf[UIPLIB_IPV6_MAX_STR_LEN];
   uiplib_ipaddr_snprint(buf, sizeof(buf), sender_addr);
@@ -108,39 +136,49 @@ PROCESS_THREAD(udp_server_process, ev, data)
 {
   PROCESS_BEGIN();
   
-  for (tp_c = 0; tp_c < (sizeof(tp) / sizeof(tp[0])); tp_c++ )
-  { 
+//  for (tp_c = 0; tp_c < (sizeof(tp) / sizeof(tp[0])); tp_c++ )
+//  { 
     //printf("after udp register\n");
-    int tp_val = tp[tp_c];
+//    int tp_val = tp[tp_c];
     int rd = NETSTACK_RADIO.set_value(RADIO_PARAM_TXPOWER, tp_val);
     rd = NETSTACK_RADIO.get_value(RADIO_PARAM_TXPOWER, &tp_val);
-    printf("P__TP %d:-:\n",tp_val);
-    printf("M__TPSTATE %d:-:M__TPSETTIME %lu\n",rd,clock_seconds());
-    int i = 1;
-    while (i <= num_conf)
-    {
-      etimer_set(&reset_timer, random_rand() % (CLOCK_SECOND*5));
-      PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&reset_timer));
-      NETSTACK_MAC.on();
-      etimer_set(&reset_timer, random_rand() % (CLOCK_SECOND*5));
-      PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&reset_timer));
+    printf("\nP__TP,%d:-:M__TPSTATE,%d:-:M__TPSETTIME,%lu\n",tp_val,rd,clock_seconds());
+    //i = 1;
+//    while (conf_count <= num_conf)
+//    {
+//      printf("\n\n_______________Configuration Number_______________%d\n\n",conf_count+(tp_c*num_conf));
+      
+      //etimer_set(&reset_timer, random_rand() % (CLOCK_SECOND*3));
+      //PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&reset_timer));
+      //NETSTACK_MAC.on();
+      //etimer_set(&reset_timer, random_rand() % (CLOCK_SECOND*7));
+      //PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&reset_timer));
     /* Initialize DAG root */
       NETSTACK_ROUTING.root_start();
-
-      etimer_set(&reset_timer, random_rand() % (CLOCK_SECOND*21));
+      //per_conf_counter = 0;
+//      etimer_set(&reset_timer, ((CLOCK_SECOND*run_delay)+skew_pad));
+      etimer_set(&reset_timer, (CLOCK_SECOND*run_delay));
       PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&reset_timer));
-      ct_start = clock_seconds();
+      //ct_start = clock_seconds();
+      printf("M__STARTTIME,%lu\n", clock_seconds());
       /* Initialize UDP connection */
       simple_udp_register(&udp_conn, UDP_SERVER_PORT, NULL,
                           UDP_CLIENT_PORT, udp_rx_callback);
 
-      printf("\nM__CONFNUM,%d:-:M__STARTTIME,%lu\n", i+(tp_c*num_conf),ct_start);
-
-      etimer_set(&reset_timer, (run_time*CLOCK_SECOND));
+      etimer_set(&reset_timer, (run_time*CLOCK_SECOND*num_conf));
       PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&reset_timer));
-      NETSTACK_MAC.off();
-    }
-  }
+//      printf("\nM__PKTSRECVD,%d:-:CONFNUM,%d\n",per_conf_counter,conf_count+(tp_c*num_conf));
+//      per_conf_counter = 0;
+      //NETSTACK_MAC.off();
+//      conf_count += 1;
+//      skew_pad += 8;
+      log_energy();
+      //NETSTACK_MAC.init();
+      //NETSTACK_MAC.on();
+      //tsch_set_coordinator(1);
+//    }
+//    conf_count = 1;
+//  }
   
   printf("M__TOTALPKTSRECVD,%d:-:",counter);
   printf("\nM__ENDTIME,%lu\n<**********>\n<**********>\n", clock_seconds());
