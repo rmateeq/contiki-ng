7cdcf0407903521c56150517be1918b722585565
rmateeq <rmateeq@yahoo.com>
2019-07-27 04:39:20 +0500

    Update udp-server.c

--- examples/rpl-udp/ateeq/csma/udp-server.c
+++ examples/rpl-udp/ateeq/csma/udp-server.c
@@ -100,7 +100,7 @@ udp_rx_callback(struct simple_udp_connection *c, const uip_ipaddr_t *sender_addr
          uint16_t receiver_port, const uint8_t *data, uint16_t datalen)
 {
   
-  uint64_t local_time_clock_ticks = (unsigned long) ((double) RTIMER_NOW()*rtimer_mul);//20UL; //tsch_get_network_uptime_ticks();
+  uint64_t local_time_clock_ticks = (unsigned long) ((double) RTIMER_NOW()*(0.030518));//20UL; //tsch_get_network_uptime_ticks();
   uint64_t remote_time_clock_ticks = extractNetworkUptime(datalen, (char *) data);
   const int countExtracted = extractCount(datalen, (char *) data);
   counter++;
