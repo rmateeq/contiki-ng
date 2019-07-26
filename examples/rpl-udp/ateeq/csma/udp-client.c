ad8c6f14ac5ca69ebf52f1f8a491ce5ea6fe20cd
rmateeq <rmateeq@yahoo.com>
2019-07-27 04:37:29 +0500

    Update udp-client.c

--- examples/rpl-udp/ateeq/csma/udp-client.c
+++ examples/rpl-udp/ateeq/csma/udp-client.c
@@ -229,7 +229,7 @@ PROCESS_THREAD(udp_client_process, ev, data)
               
               //const uint64_t network_uptime = tsch_get_network_uptime_ticks();
               char* packet = NULL;
-              packet = constructPacket(ps[ps_c], (unsigned long) ((double) RTIMER_NOW()*rtimer_mul), ++counter);
+              packet = constructPacket(ps[ps_c], (unsigned long) ((double) RTIMER_NOW()*(0.030518)), ++counter);
               free(pack);
               //printf("Message sent: %s",packet);
               //printf("M__MSGLEN %d",strlen(packet));
