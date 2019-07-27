43295ce7724887aa65b93fcf022727804e2900f9
rmateeq <rmateeq@yahoo.com>
2019-07-27 07:15:52 +0500

    Create eui64-retriever.c

---
+++ examples/eui64-retriever/eui64-retriever.c
@@ -0,0 +1,45 @@
+#include "contiki.h"
+#include "net/linkaddr.h"
+#include <stdio.h> /* For printf() */
+
+void
+print_lladdr(const linkaddr_t *lladdr)
+{
+  if(lladdr == NULL) {
+     printf("(NULL LL addr)");
+     return;
+  } else {
+    unsigned int i;
+    for(i = 0; i < LINKADDR_SIZE; i++) {
+       if(i > 0) {
+            printf("-");
+       }
+       printf("%02x", lladdr->u8[i]);
+    }
+  }
+}
+
+/*---------------------------------------------------------------------------*/
+PROCESS(eui64_retriever_process, "EUI64 Retriever");
+AUTOSTART_PROCESSES(&eui64_retriever_process);
+/*---------------------------------------------------------------------------*/
+PROCESS_THREAD(eui64_retriever_process, ev, data)
+{
+  static struct etimer timer;
+
+  PROCESS_BEGIN();
+
+  /* Setup a periodic timer that expires after 10 seconds. */
+  etimer_set(&timer, CLOCK_SECOND * 1000);
+  printf("I am in..");
+  while(1) {
+   print_lladdr(&linkaddr_node_addr);
+   printf("\n");
+
+    /* Wait for the periodic timer to expire and then restart the timer. */
+    PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&timer));
+    etimer_reset(&timer);
+  }
+
+  PROCESS_END();
+}
