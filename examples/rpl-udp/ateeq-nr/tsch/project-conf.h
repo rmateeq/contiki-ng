e4a382d469983aab3ce48da5a2a7af4a87067997
rmateeq <rmateeq@yahoo.com>
2019-08-31 00:54:08 +0500

    Update project-conf.h

--- examples/rpl-udp/ateeq-nr/tsch/project-conf.h
+++ examples/rpl-udp/ateeq-nr/tsch/project-conf.h
@@ -18,6 +18,7 @@
 #define ENERGEST_CONF_ON 1
 //#endif
 /***Logging***/
+#define LOG_CONF_LEVEL_IPV6                        LOG_LEVEL_DBG
 #define LOG_CONF_LEVEL_MAC                         LOG_LEVEL_DBG
 #define LOG_CONF_LEVEL_RPL                         LOG_LEVEL_DBG
 #endif /* PROJECT_CONF_H_ */
