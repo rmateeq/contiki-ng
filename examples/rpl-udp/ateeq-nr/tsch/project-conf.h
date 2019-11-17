301887b15f3aa50305c24149e78910c9965be449
rmateeq <rmateeq@yahoo.com>
2019-11-17 18:17:40 +0500

    Update project-conf.h

--- examples/rpl-udp/ateeq-nr/tsch/project-conf.h
+++ examples/rpl-udp/ateeq-nr/tsch/project-conf.h
@@ -1,12 +1,12 @@
 #ifndef PROJECT_CONF_H_
 #define PROJECT_CONF_H_
 //Enforce MAC_MAX_TRANSMISSIONS across all nodes
-#define UIP_CONF_TAG_TC_WITH_VARIABLE_RETRANSMISSIONS 1
+//#define UIP_CONF_TAG_TC_WITH_VARIABLE_RETRANSMISSIONS 1
 
 //#ifndef UIP_CONF_BUFFER_SIZE
 //#define UIP_CONF_BUFFER_SIZE 165
 //#endif
-#define UIP_CONF_ROUTER 0
+//#define UIP_CONF_ROUTER 0
 /*** MAC Layer ***/
 // select MAC protocol [2+1 options]
 #define NETSTACK_CONF_MAC tschmac_driver //csma_driver //nullmac_driver,
